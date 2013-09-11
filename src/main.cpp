a03bdb342b81861062c9fd3aca880992d90fcdcc
Alex Stanev <alex@stanev.org>
2013-09-11 12:55:11 +0300

    Add -q option to print only calculated keys. Suitable for scripting.

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,7 @@ int main(int argc, char * argv[]) {
     parser.addOption("-s", QCmdLineArgument::StoreValue, QObject::tr("SSID"), "--ssid", "network_name");
     parser.addOption("-m", QCmdLineArgument::StoreValue, QObject::tr("MAC address"), "--mac", "mac_address");
     parser.addOption("-v", QCmdLineArgument::StoreTrue, QObject::tr("Version"), "--version");
+    parser.addOption("-q", QCmdLineArgument::StoreTrue, QObject::tr("Print only calculated keys"), "--quiet");
     parser.addOption("--no-gui", QCmdLineArgument::StoreTrue, QObject::tr("No UI launch"));
     QString error = "";
     QVariantMap options = parser.parse(QCoreApplication::arguments(), &error);
@@ -57,21 +58,25 @@ int main(int argc, char * argv[]) {
         QString mac = options.value("m", "").toString().toUpper();
         if ( mac.length()>0 && mac.count(QRegExp("^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$")) == 0 ){
             mac = "";
-            std::cout << QObject::tr("Invalid MAC. It will not be used.").toUtf8().data() << std::endl;
+            if ( !options.value("q", false).toBool() )
+                std::cout << QObject::tr("Invalid MAC. It will not be used.").toUtf8().data() << std::endl;
         }
         QString ssid = options.value("s", "").toString();
         QScanResult wifi(ssid,mac);
         wifi.checkSupport(m);
         QVector<Keygen *> * keygens = wifi.getKeygens();
         if (keygens == NULL ){
-            std::cout << QObject::tr("Out of memory.").toUtf8().data() << std::endl;
+            if ( !options.value("q", false).toBool() )
+                std::cout << QObject::tr("Out of memory.").toUtf8().data() << std::endl;
             return -100;
         }
         if ( keygens->size() == 0){
-            std::cout << QObject::tr("Unsupported network. Check the MAC address and the SSID.").toUtf8().data() << std::endl;
+            if ( !options.value("q", false).toBool() )
+                std::cout << QObject::tr("Unsupported network. Check the MAC address and the SSID.").toUtf8().data() << std::endl;
             return -2;
         }
-        std::cout << QObject::tr("Calculating keys. This can take a while.").toUtf8().data() << std::endl;
+        if ( !options.value("q", false).toBool() )
+            std::cout << QObject::tr("Calculating keys. This can take a while.").toUtf8().data() << std::endl;
         QVector<QString> results;
         for ( int i = 0; i < keygens->size(); ++i ){
             try{
@@ -80,20 +85,24 @@ int main(int argc, char * argv[]) {
                     results.append(s);
                 }
             }catch (int e){
-                std::cout << QObject::tr("Errors while calculating.").toUtf8().data() << std::endl;
+                if ( !options.value("q", false).toBool() )
+                    std::cout << QObject::tr("Errors while calculating.").toUtf8().data() << std::endl;
                 return -3;
             }
         }
         if (results.isEmpty()) {
-            std::cout << QObject::tr("No keys were calculated.").toUtf8().data() << std::endl;
+            if ( !options.value("q", false).toBool() )
+                std::cout << QObject::tr("No keys were calculated.").toUtf8().data() << std::endl;
             return -1;
         }else{
-            std::cout << QObject::tr("Calculated Passwords for %1").arg(wifi.getSsidName()).toUtf8().data() << std::endl;
+            if ( !options.value("q", false).toBool() )
+                std::cout << QObject::tr("Calculated Passwords for %1").arg(wifi.getSsidName()).toUtf8().data() << std::endl;
             for (int i = 0; i < results.size(); ++i)
                 std::cout <<  results.at(i).toLatin1().data() << std::endl;
             return 0;
         }
     }
+
     RouterKeygen w;
     if ( !options.value("no-gui", false).toBool() )
         w.showWithDialog();
